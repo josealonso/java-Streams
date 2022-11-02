db31026e6ce91a17dd496e76fcb07abc5ea31edc
JR <jose.alonso.programmer@gmail.com>
2022-11-02 18:07:29 +0100

    Calculate the min value using the reduce operation.

--- src/main/java/streams/StreamsMinMaxExample.java
+++ src/main/java/streams/StreamsMinMaxExample.java
@@ -16,13 +16,25 @@ public class StreamsMinMaxExample {
                 .reduce((a, b) -> a > b ? a : b);
     }
 
+    public static Optional<Integer> findMinValueOptional(List<Integer> integerList) {
+        return integerList.stream()
+                .reduce((a, b) -> a < b ? a : b);
+    }
+
     public static void main(String[] args) {
         var integersList = List.of(6, 7, 8, 9, 10);
         System.out.println("The max number is: " + findMaxValue(integersList));
 
         Optional<Integer> maxValueOptional = findMaxValueOptional(integersList);
+        Optional<Integer> minValueOptional = findMinValueOptional(integersList);
         if (maxValueOptional.isPresent()) {
             System.out.println("The max number using Optional is: " + maxValueOptional.get());
+        } else {
+            System.out.println("Input list is empty");
+        }
+
+        if (minValueOptional.isPresent()) {
+            System.out.println("The min number using Optional is: " + minValueOptional.get());
         } else {
             System.out.println("Input list is empty");
         }
