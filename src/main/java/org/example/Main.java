c0219904d1442bda1593cecd263bad41236f6bf9
JR <jose.alonso.programmer@gmail.com>
2022-11-02 11:18:20 +0100

    Consumer and BiConsumer examples

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    /*
+    Lambdas are anonymous functions. They are NOT tied to any class like a regular method.
+    Lambda can also be assigned a variable and passed around.
+    It is an EXPRESSION.
+    They implement functional interfaces (Comparator, Runnable).
+     */
+    public int usingLambdas() {
+        return 1;
+    }
+}
