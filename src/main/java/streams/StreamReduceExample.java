2bed27ca97cee1d0f63b9e02f90c973e0f4c6b4e
JR <jose.alonso.programmer@gmail.com>
2022-11-02 17:16:01 +0100

    reduce and Optional

--- src/main/java/streams/StreamReduceExample.java
+++ src/main/java/streams/StreamReduceExample.java
@@ -1,5 +1,8 @@
 package streams;
 
+import data.Student;
+import data.StudentDataBase;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -9,16 +12,19 @@ to a single value
  */
 public class StreamReduceExample {
     public static int performMultiplication(List<Integer> integerList) {
-//        int multiply = integerList.stream()
-//                .reduce((a, b) -> a * b);
         return integerList.stream()
                 .reduce(1, (a, b) -> a * b);  // 1 is the accumulator
     }
-    // A function should NOT return an Optonal type
-    public static Optional<Integer> performSumWithoutIdentity(List<Integer> integerList) {
 
+    // A function should NOT return an Optional type
+    public static Optional<Integer> performSumWithoutIdentity(List<Integer> integerList) {
         return integerList.stream()
                 .reduce((a, b) -> a + b);
+    }
+
+    public static Optional<Student> getHighestGpaStudent() {
+        return StudentDataBase.getAllStudents().stream()
+                .reduce((s1, s2) -> (s1.getGpa() > s2.getGpa() ? s1 : s2));
     }
 
     public static void main(String[] args) {
@@ -30,5 +36,10 @@ public class StreamReduceExample {
 
         System.out.println("Sum is: " + sum);
         System.out.println("Multiplication is: " + performMultiplication(integersList));
+        Optional<Student> winnerStudent = getHighestGpaStudent();
+        if (winnerStudent.isPresent()) {
+            var student = winnerStudent.get();
+            System.out.println("Highest GPA student is: " + student);
+        }
     }
 }
